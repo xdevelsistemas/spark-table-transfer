b07c8a71ea6088cfddf9de54f336763a66a80b0c
pnakibar <pedro.nakibar@xdevel.com.br>
2016-03-09 14:47:50 -0300

    cleaned and ready to go!

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-README.md
+# Spark Table Transfer
+Aplicação feita em cima do Spark para transferência de tabelas de bancos de dados.
+
+# Obs:
+No momento suporta apenas bancos de dados Postgres
+
+# Como utilizar:
+Rodar com os seguintes argumentos:
+
+```bash
+java -jar tabletransfer originalHost originalDatabase originalUser originalPassword originalTable destinationHost destinationTable destinationUser destinationPasswordl destinationTable
+```
---
+++ src/main/scala/br/com/xdevel/tabletransfer/Main.scala
@@ -0,0 +1,52 @@
+package br.com.xdevel.tabletransfer
+
+/* SimpleApp.scala */
+
+import java.util.Properties
+
+import org.apache.spark.sql.{SQLContext, SaveMode}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Main extends App{
+  //copies a table from A to B
+  // original table
+  val ORIGINAL_HOST = args(0) //"localhost"
+  val ORIGINAL_DATABASE = args(1) //"postgres"
+  val ORIGINAL_USER = args(2) //"postgres"
+  val ORIGINAL_PASSWORD = args(3) //"mysecretpassword"
+  val ORIGINAL_CONNECTION_URL = s"jdbc:postgresql://$ORIGINAL_HOST/$ORIGINAL_DATABASE?user=$ORIGINAL_USER&password=$ORIGINAL_PASSWORD"
+  val ORIGINAL_TABLE = args(4) //"public.table_name"
+
+  //destination table
+  val DESTINATION_HOST = args(5) //"localhost"
+  val DESTINATION_DATABASE = args(6) //"postgres"
+  val DESTINATION_USER = args(7) //"postgres"
+  val DESTINATION_PASSWORD = args(8) //"mysecretpassword"
+  val DESTINATION_CONNECTION_URL = s"jdbc:postgresql://$DESTINATION_HOST/$DESTINATION_DATABASE?user=$DESTINATION_USER&password=$DESTINATION_PASSWORD"
+  val DESTINATION_TABLE = args(9) // "public.table_name"
+
+
+  // spark!
+  val conf = new SparkConf().setAppName("Spark Table Transfer").setMaster("local")
+  val sc = new SparkContext(conf)
+  val sqlContext = new SQLContext(sc)
+
+  // criar dataFrame da tabela e registrar ele
+  val original_table = sqlContext.read.jdbc(
+    ORIGINAL_CONNECTION_URL,
+    ORIGINAL_TABLE,
+    new Properties()
+  )
+  original_table.registerTempTable("table_name")
+
+  // pegar todos os registros
+  val results = sqlContext.sql("SELECT * FROM original_table")
+
+  //inserir no novo bd todos os registros
+  //obs ele cria a tabela se não for especificada
+  results.write.mode(SaveMode.Overwrite).jdbc(
+    DESTINATION_CONNECTION_URL,
+    DESTINATION_TABLE,
+    new Properties()
+  )
+}
